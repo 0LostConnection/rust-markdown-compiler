f543d494bd85ee06d4cbe55ba0b039ed29276276
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-07-23 13:59:52 -0300

    Learning strings - Version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn get_version() -> u16 {
+    1000
+}
+
+fn usage() {
+    // Long way
+    // let version: u16;
+    // version = get_version();
+    // println("Version {}", version)
+
+    // Short way
+    let version: u16 = get_version();
+
+    println!("Tinymd, a markdown compiler written in Rust by Geovane Saraiva da Silva");
+    println!("Version {version}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage()
 }
